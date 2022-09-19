ccbf3a981b393d3b37bb7d7373b211e0a6a52a2d
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-09-19 12:54:45 -0300

    chore: Webpack dist initial added

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  devServer: {
+    static: { directory: path.join(process.cwd(), 'dist') },
+    compress: true,
+    port: 8050,
+  },
+};

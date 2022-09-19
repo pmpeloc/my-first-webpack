72ceba16ba9275fa90d08673dafbeca2c01d5a6f
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-09-19 15:43:50 -0300

    fix: Script start fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,5 +7,13 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  devServer: {
+    static: {
+      directory: path.join(process.cwd(), 'dist'),
+    },
+    compress: true,
+    port: 8050,
   },
 };

d14112fbb16f025936c7a7582463bd43290828d7
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-09-21 11:48:38 -0300

    feat: Project finished

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import template from './components/card.html';
 window.onload = function () {
   console.log('Hello world!!!');
   console.info('Its my first webpack :)');
+  const hello = () => console.log('hell0o');
+  hello();
 
   document.getElementById('app').innerHTML = template;
 };
